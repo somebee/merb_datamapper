a134e26d89093b22b74b69590436a6114cc846ab
somebee <sindre@identu.no>
2014-06-27 15:20:24 +0200

    updated to DM 1.2

--- lib/merb_datamapper/version.rb
+++ lib/merb_datamapper/version.rb
@@ -1,6 +1,6 @@
 module Merb
   module DataMapper
     VERSION                = '1.2.0.pre1'.freeze
-    DM_VERSION_REQUIREMENT = '>= 1.0'.freeze
+    DM_VERSION_REQUIREMENT = '>= 1.2'.freeze
   end
 end
